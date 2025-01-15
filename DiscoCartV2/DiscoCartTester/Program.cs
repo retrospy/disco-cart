16a46c2d90e8d1596d046ad26c3af86be8bbf805
Christopher J Mallery <zoggins@retro-spy.com>
2025-01-15 14:12:28 -0800

    Added writing to every block on each settings to tester

--- DiscoCartV2/DiscoCartTester/Program.cs
+++ DiscoCartV2/DiscoCartTester/Program.cs
@@ -1,6 +1,5 @@
 ﻿
 
-using System.Collections.Generic;
 using System.IO.Ports;
 
 SerialPort? port;
@@ -12,86 +11,57 @@ port.Open();
 port.Write("ERA%");
 Thread.Sleep(10000);
 
-byte[] buffer = new byte[512];
-
 for (int i = 0; i < 16; ++i)
 {
-    Console.WriteLine("Set dial to " + (i + 1) + " and hit Enter...");
+    Console.WriteLine("Set dial to " + (i + 1) + " and press any key...");
     Console.ReadLine();
 
-    port.Write(string.Format("X{0:x}%", 0x3FFFFF));
-
-    for (int j = 0; j < 400000; ++j)
+    for (int j = 0; j < 40; ++j)
     {
-        port.Write(buffer, 0, 512);
+        port.Write(string.Format("W{0:x}:{1:x}%", j * 0x10000, Convert.ToString(i + 1, 16)));
     }
 }
 
-for (int i = 0; i < 1; ++i)
+char[] garbage = new char[100];
+while(port.BytesToRead > 0)
+    port.Read(garbage, 0, 100);
+
+for (int i = 0; i < 16; ++i)
 {
-    bool failed = false;
     Console.WriteLine("Set dial to " + (i + 1).ToString() + " and press any key...");
     Console.ReadLine();
 
-    port.Write(string.Format("S{0:x}%", 0x3FFFFF));
+    bool failed = false;
 
-    int index = 0;
-    do
+    for (int j = 0; j < 40; ++j)
     {
-        int cnt = port.Read(buffer, 0, 512);
-        index += cnt;
+        port.Write(string.Format("R{0:x}%", j * 0x10000, i + 1));
 
-        for(int j = 0; i < cnt; ++j)
+        char[] buffer = new char[4];
+
+        port.Read(buffer, 0, 1);
+        port.Read(buffer, 1, 1);
+        port.Read(buffer, 2, 1);
+        port.Read(buffer, 3, 1);
+        port.Read(garbage, 0, 1);
+        port.Read(garbage, 0, 1);
+
+        string val = new string(buffer);
+
+        short intVal = short.Parse(val, System.Globalization.NumberStyles.HexNumber);
+
+        if (intVal != i + 1)
         {
-            if (buffer[i] != 0)
-            {
-                failed = true;
-                break;
-            }
+            failed = true;
+            break;
         }
+    }
 
-    } while (!failed && index < ((0x3FFFFF + 1) * 2));
-
-    if (failed)
-        Console.WriteLine("FAILED");
-    else
+    if (!failed)
         Console.WriteLine("PASSED");
+    else
+        Console.WriteLine("FAILED");
 }
-
-
-//    port.Write(string.Format("W{0:x}:{1:x}%", 0, Convert.ToString(i + 1, 16)));
-//}
-
-//char[] garbage = new char[100];
-//while(port.BytesToRead > 0)
-//    port.Read(garbage, 0, 100);
-
-//for (int i = 0; i < 16; ++i)
-//{
-//    Console.WriteLine("Set dial to " + (i + 1).ToString() + " and press any key...");
-//    Console.ReadLine();
-
-//    port.Write(string.Format("R{0:x}%", 0, i + 1));
-
-//    char[] buffer = new char[4];
-
-
-//    port.Read(buffer, 0, 1);
-//    port.Read(buffer, 1, 1);
-//    port.Read(buffer, 2, 1);
-//    port.Read(buffer, 3, 1);
-//    port.Read(garbage, 0, 1);
-//    port.Read(garbage, 0, 1);
-
-//    string val = new string(buffer);
-
-//    Int16 intVal = Int16.Parse(val, System.Globalization.NumberStyles.HexNumber);
-
-//    if (intVal == i + 1)
-//        Console.WriteLine("PASSED");
-//    else
-//        Console.WriteLine("FAILED: " + intVal + " != " + (i+1).ToString());
-//}
 
 port.Close();
 
